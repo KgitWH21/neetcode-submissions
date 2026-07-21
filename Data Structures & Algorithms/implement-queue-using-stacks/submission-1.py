a31c81c3d4b069202258c62766615009d26f8686
Keith Hayden <84858282+KgitWH21@users.noreply.github.com>
2026-07-21 15:24:10 -0700

    Add: implement-queue-using-stacks - submission-1

---
+++ Data Structures & Algorithms/implement-queue-using-stacks/submission-1.py
@@ -0,0 +1,35 @@
+class MyQueue:
+
+    def __init__(self):
+        self.items = []
+        
+
+    def push(self, x: int) -> None:
+        self.items.append(x)
+
+        
+
+    def pop(self) -> int:
+        if not self.items:
+            raise Exception("Queue is empty")
+        return self.items.pop(0)
+        
+
+    def peek(self) -> int:
+        if not self.items:
+            raise Exception("Queus is empty")
+        return self.items[0]
+        
+
+    def empty(self) -> bool:
+        return True if self.items == [] else False
+
+        
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
